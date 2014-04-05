ae4ac9f6a04d8b18e3dc438a79a756c994ead1d4
Martin Jérémie <martin.jeremie42@gmail.com>
2014-04-05 16:25:07 +0200

    Add Minimax AI
    Just a dirty implementation of Minimax, without Alpha–beta

--- src/ai.rs
+++ src/ai.rs
@@ -1,38 +1,102 @@
 use rand::random;
 use game::Game;
 
-static DEPTH : int = 5;
-static MAX_VAL : int = 1000;
-static MIN_VAL : int = -1000;
+static DEPTH : int = 4;
+static INF : int = 100000;
 
 pub fn get_random_vec(game: &Game, move: ~[int]) -> (int, int)
 {
     Game::int_to_vec(move[random::<uint>() % move.len()])
 }
 
-/* Minimax with depth, I think */
-pub fn get_best_vec(game: &Game, move: ~[int]) -> (int, int)
+pub fn Minimax(game: &Game, move: ~[int]) -> (int, int)
 {
-    let &mut cpy: &Game;
-    let mut tab: [int, ..4] = [0, ..4];
-    let mut vec_i = 0;
+    let mut cpy: Game;
+    let mut best_move = (0, 0);
+    let mut val_max = -INF;
+    let mut val;
 
     for &i in move.iter()
     {
-        let (a, b, c) = (game.score, game.move_nb, game.merged_nb);
-
-        cpy = *game;
+        cpy = game.clone();
         cpy.move(Game::int_to_vec(i));
+        cpy.add_random_tile();
 
-        let (a2, b2, c2) = (cpy.score-a, cpy.move_nb-b, cpy.merged_nb-c);
-        tab[i as uint] = (a2*c2)/(b2);
+        val = min(&cpy, cpy.list_move(), DEPTH);
 
-        if tab[vec_i as uint] <= tab[i as uint]
+        if val > val_max
         {
-            vec_i = i;
+            val_max = val;
+            best_move = Game::int_to_vec(i);
         }
     }
 
-    Game::int_to_vec(vec_i)
+    best_move
+}
+
+fn min(game: &Game, move: ~[int], depth: int) -> int
+{
+    if depth == 0 || move.len() == 0
+    {
+        return eval(game, move);
+    }
+
+    let mut cpy: Game;
+    let mut val_min = INF;
+    let mut val;
+
+    for &i in move.iter()
+    {
+        cpy = game.clone();
+        cpy.move(Game::int_to_vec(i));
+        cpy.add_random_tile();
+
+        val = max(&cpy, cpy.list_move(), depth-1);
+
+        if val < val_min
+        {
+            val_min = val;
+        }
+    }
+
+    val_min
+}
+
+fn max(game: &Game, move: ~[int], depth: int) -> int
+{
+    if depth == 0 || move.len() == 0
+    {
+        return eval(game, move);
+    }
+
+    let mut cpy: Game;
+    let mut val_max = -INF;
+    let mut val;
+
+    for &i in move.iter()
+    {
+        cpy = game.clone();
+        cpy.move(Game::int_to_vec(i));
+        cpy.add_random_tile();
+
+        val = min(&cpy, cpy.list_move(), depth-1);
+
+        if val > val_max
+        {
+            val_max = val;
+        }
+    }
+
+    val_max
+}
+
+fn eval(game: &Game, move: ~[int]) -> int
+{
+    if move.len() == 0
+    {
+        return -INF + (game.score * game.merged_nb) / game.move_nb
+    }
+
+    (game.score * game.merged_nb) / game.move_nb
 }
 
